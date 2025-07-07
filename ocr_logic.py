6169de6b87a4862506d9eb2e44a55fa4732a38a7
Marios <mariosdemo2008>
2025-07-07 17:13:41 +0300

    Add workout data extraction script

--- ocr_logic.py
+++ ocr_logic.py
@@ -17,8 +17,6 @@ def extract_workout_data(image):
     # Distance (look for number before the word "Distance")
     distance_match = re.search(r"(\d+(\.\d+)?)\s*Distance", text)
 
-    # Try to find time next to "Activity Time" or just any valid time format
-    time_match = re.search(r"(?:Activity\s+Time.*?)?([0-2]?[0-9]:[0-5][0-9])", text, re.I)
 
     # Average Pace: something like "Average 3'28"
     avg_pace_match = re.search(r"Average\s*([0-9]{1,2}'[0-9]{2})", text)
@@ -26,9 +24,17 @@ def extract_workout_data(image):
     # Best Pace: something like "Best km 3'19"
     best_pace_match = re.search(r"Best\s*km\s*([0-9]{1,2}'[0-9]{2})", text)
 
+    activity_time_match = re.search(r"Activity Time\s*([0-2]?[0-9]:[0-5][0-9])", text)
+    if activity_time_match:
+        time = activity_time_match.group(1)
+    else:
+        all_times = re.findall(r"([0-2]?[0-9]:[0-5][0-9])", text)
+        time = all_times[0] if all_times else "Unknown"
+
     return {
         "distance": distance_match.group(1) + " km" if distance_match else "Unknown",
-        "time": time_match.group(1) if time_match else "Unknown",
+        "time": time,
         "pace": avg_pace_match.group(1) + "/km" if avg_pace_match else "Unknown",
         "best_pace": best_pace_match.group(1) + "/km" if best_pace_match else "Unknown"
     }
+
