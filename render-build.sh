8f2d8c449bab7e332291d70d78668a2d1b062ff6
Marios <mariosdemo2008>
2025-07-07 11:02:40 +0300

    Fix: Add Tesseract install for Render deployment

--- render-build.sh
+++ render-build.sh
@@ -1,6 +1,2 @@
 #!/usr/bin/env bash
-# Install Tesseract during the build phase
 apt-get update && apt-get install -y tesseract-ocr
-
-# Proceed with Python dependencies
-pip install -r requirements.txt
