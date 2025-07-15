3e6dae6612f9b596d2743e31c4488fad15e3caae
Marios <mariosdemo2008>
2025-07-15 13:15:04 +0300

    update files

--- parsers/coros_parser/utils/__init__.py
+++ parsers/coros_parser/utils/__init__.py
@@ -1,2 +1,15 @@
-# parsers/coros_parser/utils/__init__.py
-# Empty file
+import re
+
+def parse_time_to_sec(t):
+    try:
+        parts = re.split(r"[:.]", t)
+        return int(parts[0]) * 60 + float(parts[1])
+    except:
+        return 0
+
+def pace_to_seconds(p):
+    try:
+        parts = p.replace("’", "'").replace("`", "'").split("'")
+        return int(parts[0]) * 60 + int(parts[1])
+    except:
+        return None
